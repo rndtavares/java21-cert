2630208e5cb4ee4d805489abf0c8642951856fe2
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-03-12 09:32:12 -0300

    first commit

---
+++ src/main/java/dev/ronaldotavares/java21/building_blocks/Literals.java
@@ -0,0 +1,16 @@
+package dev.ronaldotavares.java21.building_blocks;
+
+public class Literals {
+    public static void main(String[] args) {
+        testLiterals();
+    }
+
+    public static void testLiterals() {
+        int testBinary = 0b10;
+        int testOctal = 017;
+        int testHex = 0xFF;
+        System.out.println(testBinary);
+        System.out.println(testOctal);
+        System.out.println(testHex);
+    }
+}
