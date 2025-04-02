57bce6ef61027a509e3ead0dd7767112fe2021e8
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-04-01 21:14:25 -0300

    2.3 and 2.4

--- src/main/java/dev/ronaldotavares/java21/_2_operators/_2_3_WorkingWithBinaryArithmeticOperators.java
+++ src/main/java/dev/ronaldotavares/java21/_2_operators/_2_3_WorkingWithBinaryArithmeticOperators.java
@@ -1,4 +1,82 @@
 package dev.ronaldotavares.java21._2_operators;
 
 public class _2_3_WorkingWithBinaryArithmeticOperators {
+    public static void main(String[] args) {
+        arithmeticOperators();
+        addingParentesis();
+        verifyingParenthesesSyntax();
+        divisionAndModulusOperators();
+        numericPromotionRules();
+    }
+
+    private static void arithmeticOperators() {
+        System.out.println("arithmeticOperators");
+        int price = 2 * 5 + 3 * 4 -8;
+        System.out.println(price);
+        int priceEquivalent = 10 + 12 - 8;
+        System.out.println(priceEquivalent);
+    }
+
+    private static void addingParentesis() {
+        System.out.println("addingParentesis");
+        int price = 2 * ((5 + 3) * 4 - 8);
+        System.out.println(price);
+        int priceStep1 = 2 * (8 * 4 - 8);
+        System.out.println(priceStep1);
+        int priceStep2 = 2 * (32 - 8);
+        System.out.println(priceStep2);
+        int priceStep3 = 2 * 24;
+        System.out.println(priceStep3);
+    }
+
+    private static void verifyingParenthesesSyntax() {
+//        long pigeon = 1 + ((3 * 5) / 3; // DOES NOT COMPILE
+//         int blueJay = (9 + 2) + 3) / (2 * 4; // DOES NOT COMPILE
+//        short robin = 3 + [(4 * 2) + 4]; // DOES NOT COMPILE
+    }
+
+    private static void divisionAndModulusOperators() {
+        System.out.println("divisionAndModulusOperators");
+        System.out.println(9 / 3); // 3
+        System.out.println(9 % 3); // 0
+        System.out.println(10 / 3); // 3
+        System.out.println(10 % 3); // 1
+        System.out.println(11 / 3); // 3
+        System.out.println(11 % 3); // 2
+        System.out.println(12 / 3); // 4
+        System.out.println(12 % 3); // 0
+
+        System.out.println(2 % 5); // 2
+        System.out.println(7 % 5); // 2
+        System.out.println(2 % -5); // 2
+        System.out.println(7 % -5); // 2
+        System.out.println(-2 % 5); // -2
+        System.out.println(-7 % 5); // -2
+        System.out.println(-2 % -5); // -2
+        System.out.println(-7 % -5); // -2
+    }
+
+    private static void numericPromotionRules() {
+        System.out.println("numericPromotionRules");
+        int x = 1;
+        long y = 33;
+        var z = x * y;
+        System.out.println(z);
+
+        double x1 = 39.21;
+        float y1 = 2.1f;
+        var z1 = x1 + y1;
+        System.out.println(z1);
+
+        short x2 = 10;
+        short y2 = 3;
+        var z2 = x2 * y2;
+        System.out.println(z2);
+
+        short w3 = 14;
+        float x3 = 13;
+        double y3 = 30;
+        var z3 = w3 * x3 / y3;
+        System.out.println(z3);
+    }
 }
--- src/main/java/dev/ronaldotavares/java21/_2_operators/_2_4_AssigningOperators.java
+++ src/main/java/dev/ronaldotavares/java21/_2_operators/_2_4_AssigningOperators.java
@@ -3,7 +3,30 @@ package dev.ronaldotavares.java21._2_operators;
 public class _2_4_AssigningOperators {
 
     public static void main(String[] args) {
+        assignmentOperator();
+        castingOperator();
         primitiveAssignments();
+        appliyingCast();
+        overflowUnderflow();
+        castingValuesAndVariables();
+        compoundAssignmentOperators();
+        returnValueOfAssignmentOperator();
+    }
+
+    private static void assignmentOperator() {
+        int herd =1;
+    }
+
+    private static void castingOperator() {
+        int fur = (int)5;
+        int hair = (short)2;
+        String type = (String)"Bird";
+        short tail = (short)(4 + 10);
+//        long feathers = 10(long); // DOES NOT COMPILE
+
+//        float egg = 2.0 / 9; // DOES NOT COMPILE
+//        int tadpole = (int)5 * 2L; // DOES NOT COMPILE
+//        short frog = 3 - 2.0;  // DOES NOT COMPILE
     }
 
     private static void primitiveAssignments(){
@@ -19,6 +42,38 @@ public class _2_4_AssigningOperators {
         System.out.println("testInt " + testInt);
         System.out.println("testShort " + testShort);
         System.out.println("testLong " + testLong);
+        System.out.println("testFloat " + testFloat);
+        System.out.println("testDouble " + testDouble);
+
+//        int fish = 1.0; // DOES NOT COMPILE
+//        short bird = 1921222; // DOES NOT COMPILE
+//        int mammal = 9f;  // DOES NOT COMPILE
+//        long reptile = 192_301_398_193_810_323; // DOES NOT COMPILE
+    }
+
+    private static void appliyingCast() {
+        int fish = (int)1.0;
+        short bird = (short)1921222; // Stored as 20678
+        int mammal = (int)9f;
+
+        short mouse = 10;
+        short hamster = 3;
+
+        short capybara = (short)(mouse * hamster);
+
+//        short capybara1 = (short)mouse * hamster;// DOES NOT COMPILE
+//        short capybara2 = 1 + (short)(mouse * hamster); // DOES NOT COMPILE
+    }
+
+    private static void overflowUnderflow() {
+//        long reptile = (long)192301398193810323; // DOES NOT COMPILE
+        long reptile = 192301398193810323L;
+        System.out.print(2147483647+1); // -2147483648
+
+        int intOverflowOk = 2147483647+1;
+//        int intOverflowNOk = 2147483648;
+//        byte byteOverflowNotOk = 127+1;
+        byte byteOverflowOk = (byte)(127+1);
     }
 
     private static void castingValuesAndVariables(){
@@ -26,18 +81,36 @@ public class _2_4_AssigningOperators {
         byte gloves = 7 * 10;
         short scarf = 5;
         short boots = 2 + 1;
+
 //        short boots = 2 + hat; // DOES NOT COMPILE
 //        byte gloves = 7 * 100; // DOES NOT COMPILE
+
 //        byte gloves1 = 7 * 100;
 //        byte gloves2 = 700;
         byte gloves3 = (byte)700;
 
     }
 
-    private static void primitiveOverflow(){
-        int intOverflowOk = 2147483647+1;
-//        int intOverflowNOk = 2147483648;
-//        byte byteOverflowNotOk = 127+1;
-        byte byteOverflowOk = (byte)(127+1);
+    private static void compoundAssignmentOperators() {
+        int camel = 2, giraffe = 3;
+        camel = camel * giraffe; // Simple assignment operator
+        camel *= giraffe; // Compound assignment operator
+
+        long goat = 10;
+        int sheep = 5;
+//        sheep = sheep * goat; // DOES NOT COMPILE
+        sheep *= goat;
+    }
+
+
+    private static void returnValueOfAssignmentOperator() {
+        long wolf = 5;
+        long coyote = (wolf = 3);
+        System.out.println(wolf); // 3
+        System.out.println(coyote); // 3
+
+        boolean healthy = false;
+        if(healthy = true)
+            System.out.print("Good!");
     }
 }
