6b45f868630d30b9e51d3baa2f37258369be4f80
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-04-04 10:00:57 -0300

    2.5 and 2.6

--- src/main/java/dev/ronaldotavares/java21/_2_operators/_2_5_ComparingValues.java
+++ src/main/java/dev/ronaldotavares/java21/_2_operators/_2_5_ComparingValues.java
@@ -1,4 +1,118 @@
 package dev.ronaldotavares.java21._2_operators;
 
+import java.io.File;
+
 public class _2_5_ComparingValues {
+    public static void main(String[] args) {
+        equalityOperators();
+        numericComparisonOperators();
+        instanceofOperator();
+        logicalOperators();
+        bitwiseOperators();
+        conditionalOperators();
+    }
+
+    private static void equalityOperators() {
+//        boolean monkey = true == 3;  // DOES NOT COMPILE
+//        boolean ape = false != "Grape"; // DOES NOT COMPILE
+//        boolean gorilla = 10.2 == "Koko"; // DOES NOT COMPILE
+
+        boolean bear = false;
+        boolean polar = (bear = true);
+        System.out.println(polar); // true
+
+        var monday = new File("schedule.txt");
+        var tuesday = new File("schedule.txt");
+        var wednesday = tuesday;
+        System.out.println(monday == tuesday);// false
+        System.out.println(tuesday == wednesday); // true
+
+        System.out.print(null == null); // true
+    }
+
+    private static void numericComparisonOperators() {
+        int gibbonNumFeet = 2, wolfNumFeet = 4, ostrichNumFeet = 2;
+        System.out.println(gibbonNumFeet < wolfNumFeet);      // true
+        System.out.println(gibbonNumFeet <= wolfNumFeet);     // true
+        System.out.println(gibbonNumFeet >= ostrichNumFeet);  // true
+        System.out.println(gibbonNumFeet > ostrichNumFeet);   // false
+    }
+
+    private static void instanceofOperator() {
+        Integer zooTime = Integer.valueOf(9);
+        Number num = zooTime;
+        Object obj = zooTime;
+
+        openZoo(1);
+
+        System.out.println(null instanceof Object); // false
+        String noObjectHere = null;
+        System.out.println(noObjectHere instanceof String);  // false
+
+//        System.out.print(null instanceof null);  // DOES NOT COMPILE
+    }
+
+    public static void openZoo(Number time) {
+        if (time instanceof Integer)
+            System.out.print((Integer)time + " O'clock");
+        else
+            System.out.print(time);
+
+//        if(time instanceof String) // DOES NOT COMPILE
+//            System.out.print(time);
+    }
+
+
+    private static void logicalOperators() {
+        boolean eyesClosed = true;
+        boolean breathingSlowly = true;
+        boolean resting = eyesClosed | breathingSlowly;
+        boolean asleep = eyesClosed & breathingSlowly;
+        boolean awake = eyesClosed ^ breathingSlowly;
+        System.out.println(resting);  // true
+        System.out.println(asleep);   // true
+        System.out.println(awake);    // false
+    }
+
+    private static void bitwiseOperators() {
+        int number = 70;
+        System.out.println(number); // 70
+        System.out.println(number & number);   // 70
+        System.out.println(number | number);   // 70
+
+        int negated = ~number;
+        System.out.println(negated);           // -71
+        System.out.println(number & negated);  // 0
+        System.out.println(number | negated);  // -1
+
+        System.out.println(number ^ number);   // 0
+        System.out.println(number ^ negated);  // -1
+    }
+
+    private static void conditionalOperators() {
+        int hour = 10;
+        boolean zooOpen = true || (hour < 4);
+        System.out.println(zooOpen);  // true
+
+        Duck duck = new Duck();
+
+        if(duck != null & duck.getAge() < 5) { // Could throw a NullPointerException
+            // Do something
+        }
+
+        if(duck != null && duck.getAge()<5) {
+            // Do something
+        }
+
+        int rabbit = 6;
+        boolean bunny = (rabbit>= 6) || (++rabbit <= 7);
+        System.out.println(bunny);
+        System.out.println(rabbit);
+    }
+}
+class Duck{
+    int age;
+    public int getAge() {
+        return age;
+    }
 }
--- src/main/java/dev/ronaldotavares/java21/_2_operators/_2_6_MakingDecisionsWithTheTernaryOperator.java
+++ src/main/java/dev/ronaldotavares/java21/_2_operators/_2_6_MakingDecisionsWithTheTernaryOperator.java
@@ -1,4 +1,42 @@
 package dev.ronaldotavares.java21._2_operators;
 
 public class _2_6_MakingDecisionsWithTheTernaryOperator {
+
+    public static void main(String[] args) {
+        ternaryOperator();
+    }
+
+    private static void ternaryOperator() {
+        int owl = 5;
+        int food;
+        if(owl < 2) {
+            food = 3;
+        } else {
+            food = 4;
+        }
+        System.out.println(food);  // 4
+
+        int owl0 = 5;
+        int food0 = owl0 < 2 ? 3 : 4;
+        System.out.println(food0);  // 4
+
+        int food1 = owl < 4 ? owl> 2 ? 3 : 4 : 5;
+        int food2 = (owl < 4 ? ((owl> 2) ? 3 : 4) : 5);
+        System.out.println(food1);
+        System.out.println(food2);
+
+        int stripes = 7;  
+        System.out.print((stripes> 5) ? 21 : "Zebra");  
+//        int animal = (stripes < 9) ? 3 : "Horse";  // DOES NOT COMPILE
+
+        int sheep = 1;
+        int zzz = 1;
+        int sleep = zzz<10 ? sheep++ : zzz++;
+        System.out.print(sheep + "," + zzz);  // 2,1
+
+        int sheep1 = 1;
+        int zzz1 = 1;
+        int sleep1 = sheep1 >=10 ? sheep1++ : zzz1++;
+        System.out.print(sheep1 + "," + zzz1);  // 1,2
+    }
 }
