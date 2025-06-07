8251a877b1b618e4e3c98ced3ecab8881339149a
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-06-07 20:39:58 -0300

    13.6 part 2

--- src/main/java/dev/ronaldotavares/java21/_13_concurrency/_13_6_WorkingWithParallelStreams.java
+++ src/main/java/dev/ronaldotavares/java21/_13_concurrency/_13_6_WorkingWithParallelStreams.java
@@ -1,8 +1,10 @@
 package dev.ronaldotavares.java21._13_concurrency;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -81,6 +83,15 @@ public class _13_6_WorkingWithParallelStreams {
     void processingParallelReductions(){
         System.out.println("Processing Parallel Reductions");
 
+        performingOrderBasedTasks();
+        combiningResultsWithReduce();
+        combiningResultsWithCollect();
+        requirementsForParallelReductionWithCollect();
+    }
+
+    void performingOrderBasedTasks(){
+        System.out.println("Performing Order-Based Tasks");
+
         System.out.println("findAny (1-6) in parallel:");
         System.out.println(List.of(1, 2, 3, 4, 5, 6)
                 .parallelStream()
@@ -98,5 +109,106 @@ public class _13_6_WorkingWithParallelStreams {
         List.of(1, 2, 3, 4, 5, 6).stream().unordered().forEach(System.out::println);
         System.out.println("1-6 in parallel, unordered:");
         List.of(1, 2, 3, 4, 5, 6).stream().unordered().parallel().forEach(System.out::println);
+    }
+
+    void combiningResultsWithReduce(){
+        System.out.println("Combining Results with reduce");
+
+        System.out.println(List.of('w', 'o', 'l', 'f')
+                .parallelStream()
+                .reduce("",
+                        (s1, c) -> s1 + c,
+                        (s2, s3) -> s2 + s3)); // "wolf"
+
+        System.out.println("problematic acumulator");
+        System.out.println(List.of(1, 2, 3, 4, 5, 6)
+                .parallelStream()
+                .reduce(0, (a, b) -> (a - b)));
+
+        System.out.println("problematic identity");
+        System.out.println(List.of("w", "o", "l", "f")
+                .parallelStream()
+                .reduce("X", String::concat)); // "XwXoXlXf"
+
+    }
+
+    void combiningResultsWithCollect(){
+        System.out.println("Combining Results with collect");
+
+        Stream<String> stream = Stream.of("w", "o", "l", "f").parallel();
+        SortedSet<String> set = stream.collect(ConcurrentSkipListSet::new,
+                Set::add,
+                Set::addAll);
+        System.out.println(set);  // [f, l, o, w]
+    }
+
+    void requirementsForParallelReductionWithCollect(){
+        System.out.println("Requirements for Parallel Reduction with collect");
+//        parallelStream.collect(Collectors.toSet()); // Not a parallel reduction
+
+        parallelReductionWithToConcurrentMap();
+        parallelReductionWithGroupingByConcurrent();
+        avoidingStatefulStreams();
+    }
+
+    void parallelReductionWithToConcurrentMap(){
+        System.out.println("Parallel Reduction with toConcurrentMap");
+
+        Stream<String> ohMy = Stream.of("lions", "tigers", "bears").parallel();
+        ConcurrentMap<Integer, String> map = ohMy
+                .collect(Collectors.toConcurrentMap(String::length,
+                        k -> k,
+                        (s1, s2) -> s1 + "," + s2));
+        System.out.println(map); // {5=lions,bears, 6=tigers}
+        System.out.println(map.getClass()); // java.util.concurrent.ConcurrentHashMap
+    }
+
+    void parallelReductionWithGroupingByConcurrent(){
+        System.out.println("Parallel Reduction with groupingByConcurrent");
+
+        var ohMy = Stream.of("lions", "tigers", "bears").parallel();
+        ConcurrentMap<Integer, List<String>> map = ohMy.collect(
+                Collectors.groupingByConcurrent(String::length));
+        System.out.println(map); // {5=[lions, bears], 6=[tigers]}
+    }
+
+    void avoidingStatefulStreams(){
+        System.out.println("Avoiding Stateful Streams");
+
+        System.out.println("with serial stream and stateful operation");
+        var sertialList = addValues(IntStream.range(1, 11));
+        System.out.print(sertialList); // [2, 4, 6, 8, 10]
+
+        System.out.println();
+
+        System.out.println("with parallel stream and stateful operation");
+        var parallelList = addValues(IntStream.range(1, 11).parallel());
+        System.out.print(parallelList); // [6, 8, 10, 2, 4]
+
+        System.out.println();
+
+        System.out.println("with serial stream and not stateful operation");
+        var sertialListBetter = addValuesBetter(IntStream.range(1, 11));
+        System.out.print(sertialListBetter); // [2, 4, 6, 8, 10]
+
+        System.out.println();
+
+        System.out.println("with parallel stream and not stateful operation");
+        var parallelBetterList = addValuesBetter(IntStream.range(1, 11).parallel());
+        System.out.print(parallelBetterList); // [2, 4, 6, 8, 10]
+
+    }
+
+    List<Integer> addValues(IntStream source) {
+        var data = Collections.synchronizedList(new ArrayList<Integer>());
+        source.filter(s -> s % 2 == 0)
+                .forEach(i -> { data.add(i); }); // STATEFUL: DON'T DO THIS!
+        return data;
+    }
+
+    public List<Integer> addValuesBetter(IntStream source) {
+        return source.filter(s -> s % 2 == 0)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
