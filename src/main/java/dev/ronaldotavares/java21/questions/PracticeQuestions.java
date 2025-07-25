fded78bce23d4dd76658b66d26bdd130d653dfbe
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-07-25 10:00:30 -0300

    stream reduce refactor

--- src/main/java/dev/ronaldotavares/java21/questions/PracticeQuestions.java
+++ src/main/java/dev/ronaldotavares/java21/questions/PracticeQuestions.java
@@ -126,9 +126,7 @@ class StreamReduce {
         var result = items.parallelStream()
                 .reduce(items.parallelStream()
                                 .reduce("",
-                                        (a, b) -> a + b,
                                         (a, b) -> a + b),
-                        (a, b) -> a + b,
                         (a, b) -> a + b);
 
         System.out.println(result);
