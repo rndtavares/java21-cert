7d20cbafb2ec07d1d81617ddf0c0fe4e17bc8205
Ronaldo Tavares da Silva <rtsilva@uolinc.com>
2025-10-08 20:49:21 -0300

    Q7.13 - enum with switch

--- src/main/java/dev/ronaldotavares/java21/_7_beyond_classes/_7_ReviewQuestions.java
+++ src/main/java/dev/ronaldotavares/java21/_7_beyond_classes/_7_ReviewQuestions.java
@@ -2,11 +2,21 @@ package dev.ronaldotavares.java21._7_beyond_classes;
 
 public class _7_ReviewQuestions {
     public static void main(String[] args) {
+        _13();
         _15();
     }
 
     private static void _15(){
+        System.out.println("question 15:");
         Ghost.main(null);
+    }
+    private static void _13(){
+        System.out.println("question 13:");
+        try{
+            Weather.main(null);
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
 
@@ -25,3 +35,20 @@ class Ghost {
         StringBuilder sb = new StringBuilder();
     }
 }
+
+class Weather {
+    enum Seasons {
+        WINTER, SPRING, SUMMER, FALL
+    }
+
+    public static void main(String[] args) {
+        Seasons v = null;
+        int i = 1;
+        switch (v) {
+            case Seasons.SPRING -> System.out.print("s");
+            case Seasons.WINTER -> System.out.print("w");
+//            case null -> System.out.println("null");
+            case Seasons.SUMMER -> System.out.print("m");
+            default -> System.out.println("missing data");
+        }
+    }  }
